479ecc12058c73d77ba9640cc07343726b577fb7
Ivanor1505 <ivanor1505@gmail.com>
2023-08-26 10:50:15 +0300

    React HW2.2 ok

--- src/components/Contact/Contact.styled.js
+++ src/components/Contact/Contact.styled.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const Button = styled.button`
 display: block;
-height: 35px;
+margin-top: 10px;
+height: 25px;
 `;
 
 export const CotactItem = styled.li`
